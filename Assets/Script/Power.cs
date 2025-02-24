776f6fd8d2f9fe78c569273891af8cb3a7c254de
Abhijithbiju2002 <abhiju7@gmail.com>
2025-02-24 10:31:07 +0530

    c 2

---
+++ Assets/Script/Power.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpSpawner spawner;
+    public PowerUpSpawner.PowerUpType powerUpType;
+    public float duration = 10f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Snake"))
+        {
+            SnakeController snake = collision.GetComponent<SnakeController>();
+
+            if (snake != null)
+            {
+                switch (powerUpType)
+                {
+                    case PowerUpSpawner.PowerUpType.Shield:
+                        snake.ActivateShield(duration);
+                        break;
+                    case PowerUpSpawner.PowerUpType.SpeedBoost:
+                        snake.ActivateSpeedBoost(duration);
+                        break;
+                    case PowerUpSpawner.PowerUpType.ScoreBoost:
+                        snake.ActivateScoreBoost(duration);
+                        break;
+                }
+            }
+
+            spawner.PowerUpCollected();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Script/PowerUpSpawner.cs
+++ Assets/Script/PowerUpSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PowerUpSpawner : MonoBehaviour
@@ -8,54 +9,42 @@ public class PowerUpSpawner : MonoBehaviour
         SpeedBoost,
         ScoreBoost
     }
-    public BoxCollider2D spawnArea;
-    public PowerUpType powerUpType;
-    public float duration = 5f;
 
-    public float minSpawnTime = 10f; // Minimum time between power-ups
-    public float maxSpawnTime = 30f; // Maximum time between power-ups
+    public GameObject powerUpPrefab; // Prefab reference for power-ups
+    public BoxCollider2D spawnArea;  // The area where power-ups spawn
+    public float respawnTime = 30f;  // Respawn time for power-ups
 
-
+    private GameObject currentPowerUp;
     private void Start()
     {
-        RandomizePosition();
+        StartCoroutine(SpawnPowerUpLoop()); // Start continuous power-up spawning
     }
 
-    void RandomizePosition()
+    IEnumerator SpawnPowerUpLoop()
     {
-        Bounds bounds = this.spawnArea.bounds;
-
-        float x = Random.Range(bounds.min.y, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Snake"))
+        while (true)
         {
-            SnakeController snake = collision.GetComponent<SnakeController>();
-
-            if (snake != null)
-            {
-                switch (powerUpType)
-                {
-                    case PowerUpType.Shield:
-                        snake.ActivateShield(duration);
-                        break;
-                    case PowerUpType.SpeedBoost:
-                        snake.ActivateSpeedBoost(duration);
-                        break;
-                    case PowerUpType.ScoreBoost:
-                        snake.ActivateScoreBoost(duration);
-                        break;
-                }
-            }
-
-            Destroy(gameObject); // Remove power-up after pickup
+            yield return new WaitForSeconds(respawnTime);
+            SpawnPowerUp();
         }
     }
+
+    void SpawnPowerUp()
+    {
+        Vector3 spawnPos = GetRandomPosition();
+        GameObject newPowerUp = Instantiate(powerUpPrefab, spawnPos, Quaternion.identity);
+        newPowerUp.GetComponent<PowerUp>().spawner = this; // Link to spawner
+    }
+
+    public Vector3 GetRandomPosition()
+    {
+        Bounds bounds = spawnArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        return new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+    public void PowerUpCollected()
+    {
+        currentPowerUp = null; // Reset the tracker when collected
+    }
 }
-
-
